da4c3ea8c33931d8c509b44bf26ae3144661569b
alexssico <alexssico@gmail.com>
2016-09-02 23:36:56 +0300

    add js and css folder

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var getUser = function(){
+		VK.api('users.get', {}, function (data){
+			userId = data['response']['0']['id'];
+		}) 
+	};
+
+var getAlbum = function (){
+	VK.api('photos.getAlbums',{ owner_id: userId}, function (data){
+		album = data['response'];
+	})
+};	
+
+  VK.init(function() { 
+     getUser();
+     getAlbum(); 
+  }, function() { 
+     console.log('error');
+}, '5.53'); 
